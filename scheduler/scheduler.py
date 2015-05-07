e51f5082cdcdf8f1a4d6151961e2efe341f21a1c
Reshef Elisha <reshef.elisha@gmail.com>
2015-05-06 22:26:06 -0400

    rearranged helper methods, removed flask use

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -1,20 +1,22 @@
-from Flask import flask, render_template
 from geopy.distance import vincenty
 
-app = Flask(__name__)
+def calc(location, possible_dates):
+    return get_best_weekend(location, possible_dates)
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
+def get_best_weekend_index(location, possible_dates):
+    #given a location and a date, it finds the best one based on score
+    scores = get_scores(location, possible_dates)
+    return scores.index(min(scores))
 
-@app.route('/calc', methods=['POST'])
-def calc():
-    best_date = get_best_weekend(#location, possible dates array
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
+def get_scores(location, possible_dates):
+    #returns an array of scores for the given location at each date
+    scores=[]
+    for index in possible_dates:
+        if(hackathons[index]>0):
+            scores.append(get_weekend_score(location,index))
+        else:
+            scores.append(0)
+    return scores
 
 def get_weekend_score(location, index):
     #returns the score for a given date based on the hackathons in it
@@ -22,18 +24,3 @@ def get_weekend_score(location, index):
     for hackathon in weekends[index]:
         scores.append( 1000/(vincenty( hackathon['location'], location ).miles) )
     return sum(distances)
-
-def get_scores(location, possible_dates):
-    #returns an array of scores for the given location at each date
-    scores=[]
-    for index in possible_dates:
-        if(hackathons[index]>0):
-            scores.append(get_min_distance(location,index))
-        else:
-            scores.append(0)
-    return scores
-
-def get_best_weekend(location, possible_dates):
-    #given a location and a date, it finds the best one based on score
-    scores = get_scores(location, possible_dates)
-    return dates[scores.index(min(scores))]
